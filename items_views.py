b70d28a50486f8c9656adf97ee9538f4369e0cef
MatveyMahlyshev <matthewfootballer11@gmail.com>
2025-03-01 17:25:47 +0300

    modules

---
+++ items_views.py
@@ -0,0 +1,22 @@
+from fastapi import Path, APIRouter
+from typing import Annotated
+router = APIRouter(prefix="/items", tags=["Items"])
+
+@router.get("/")
+def list_items():
+    return ["item1", "item2"]
+
+@router.get("/latest/")
+def get_latest_item():
+    return {
+        "item": {
+            "id": "0",
+            "name": "latest",
+        }
+    }
+
+@router.get("/{item_id}/")
+def get_item_by_id(item_id: Annotated[int, Path(ge=1, lt=1_000_000)]):
+    return {
+        "item": f"item{item_id}"
+    }
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
 import uvicorn
-from pydantic import EmailStr, BaseModel
-
-class CreateUser(BaseModel):
-    email: EmailStr
-
+from items_views import router as items_router
+from users.views import router as users_router
 
 app = FastAPI()
+
+app.include_router(items_router)
+app.include_router(users_router)
 
 @app.get("/")
 def hello_index():
@@ -21,12 +21,6 @@ def hello(name: str = "World"):
         "message": f"Hello {name}"
     }
 
-@app.post("/users/")
-def create_user(user: CreateUser):
-    return {
-        "message": "success",
-        "email": user.email
-    }
 
 @app.post("/calc/add/")
 def add(a: int, b: int):
@@ -36,25 +30,6 @@ def add(a: int, b: int):
         "result": a + b,
     }
 
-@app.get("/items/")
-def list_items():
-    return ["item1", "item2"]
-
-
-@app.get("/items/latest/")
-def get_latest_item():
-    return {
-        "item": {
-            "id": "0",
-            "name": "latest",
-        }
-    }
-
-@app.get("/items/{item_id}/")
-def get_item_by_id(item_id: int):
-    return {
-        "item": f"item{item_id}"
-    }
 
 
 
