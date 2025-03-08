408929126ff07f68afe19bbaa1c72e1336c61b1d
MatveyMahlyshev <matthewfootballer11@gmail.com>
2025-03-08 19:04:59 +0300

    +user model, new migration with user table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -5,7 +5,9 @@ __all__ = (
     "Product",
     "db_helper",
     "DataBaseHelper",
+    "User",
 )
 from .base import Base
 from .product import Product
 from .db_helper import db_helper, DataBaseHelper
+from .user import User
