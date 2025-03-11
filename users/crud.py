d5af6fc31dd2dbc725b507bfc21f63672ff54a57
MatveyMahlyshev <matthewfootballer11@gmail.com>
2025-03-11 18:46:47 +0300

    refactoring app/crud.py > users/crud.py

--- users/crud.py
+++ users/crud.py
@@ -1,9 +1,104 @@
-from users.schemas import CreateUser
+import asyncio
+from core.models import User, Profile, Post
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload, selectinload
 
 
-def create_user(user_in: CreateUser) -> dict:
-    user = user_in.model_dump()
-    return {
-        "success": True,
-        "user": user,
-    }
+async def create_user(session: AsyncSession, username: str) -> User:
+    user = User(username=username)
+    session.add(user)
+    await session.commit()
+    return user
+
+
+async def get_user_by_username(session: AsyncSession, username: str) -> User | None:
+    stmt = select(User).where(User.username == username)
+    user: User | None = await session.scalar(statement=stmt)
+    if user:
+        print("found user", username, user)
+    return user
+
+
+async def create_profile(
+    session: AsyncSession,
+    user_id: int,
+    first_name: str | None = None,
+    last_name: str | None = None,
+) -> Profile:
+
+    profile = Profile(
+        user_id=user_id,
+        first_name=first_name,
+        last_name=last_name,
+    )
+    session.add(profile)
+    await session.commit()
+    return profile
+
+
+async def show_users_with_profiles(session: AsyncSession) -> list[User]:
+    stmt = select(User).options(joinedload(User.profile)).order_by(User.id)
+    users = await session.scalars(statement=stmt)
+    for user in users:
+        print(user)
+        print(user.profile.first_name)
+
+
+async def create_post(
+    session: AsyncSession, user_id: int, *post_titles: str
+) -> list[Post]:
+
+    posts = [Post(title=title, user_id=user_id) for title in post_titles]
+    session.add_all(posts)
+    await session.commit()
+    return posts
+
+
+async def get_users_with_posts(session: AsyncSession):
+    stmt = select(User).options(selectinload(User.posts)).order_by(User.id)
+    users = await session.scalars(statement=stmt)
+    for user in users:
+        print("**" * 10)
+        print(user)
+        for post in user.posts:
+            print("-", post)
+
+
+async def get_users_with_posts_and_profile(session: AsyncSession):
+    stmt = (
+        select(User)
+        .options(joinedload(User.profile), selectinload(User.posts))
+        .order_by(User.id)
+    )
+    users = await session.scalars(statement=stmt)
+    for user in users:
+        print("**" * 10)
+        print(user, user.profile and user.profile.last_name)
+        for post in user.posts:
+            print("-", post)
+
+
+async def get_profiles_with_users_and_users_with_posts(session: AsyncSession):
+    stmt = (
+        select(Profile)
+        .join(Profile.user)
+        .options(joinedload(Profile.user).selectinload(User.posts))
+        .order_by(Profile.id)
+        .where(User.username == "Artem")
+    )
+    profiles = await session.scalars(statement=stmt)
+    for profile in profiles:
+        print(profile.first_name, profile.user)
+        print(profile.user.posts)
+
+
+async def get_posts_with_authors(session: AsyncSession, user_id: int) -> list[Post]:
+    stmt = (
+        select(Post)
+        .options(selectinload(Post.user))
+        .where(Post.user_id == user_id)
+        .order_by(Post.title)
+    )
+    posts = list(await session.scalars(statement=stmt))
+    return posts
