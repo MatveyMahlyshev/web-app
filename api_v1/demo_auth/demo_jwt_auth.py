4803b4b7e31f961d75b3212e76254f8dc1918891
MatveyMahlyshev <matthewfootballer11@gmail.com>
2025-05-05 18:10:54 +0300

    jwt refresh view

--- api_v1/demo_auth/demo_jwt_auth.py
+++ api_v1/demo_auth/demo_jwt_auth.py
@@ -12,26 +12,37 @@ from .auth_helpers import (
     validate_auth_user,
     get_current_active_auth_user,
     get_current_token_payload,
+    create_access_token,
+    create_refresh_token,
+    http_bearer,
+    get_current_auth_user_for_refresh,
 )
 
 router = APIRouter(
     prefix="/jwt",
     tags=["JWT"],
+    dependencies=[Depends(http_bearer)],
 )
 
 
 @router.post("/login/", response_model=Token)
 def auth_user_jwt(user: UserAuthSchema = Depends(validate_auth_user)):
-    jwt_payload = {
-        "sub": user.username,
-        "username": user.username,
-        "email": user.email,
-    }
-    token = auth_utils.encode_jwt(jwt_payload)
+    access_token = create_access_token(user=user)
+    refresh_token = create_refresh_token(user=user)
     return Token(
-        access_token=token,
-        token_type="Bearer",
+        access_token=access_token,
+        refresh_token=refresh_token,
     )
+
+
+@router.post(
+    "/refresh/",
+    response_model=Token,
+    response_model_exclude_none=True,
+)
+def auth_refresh_jwt(user: UserAuthSchema = Depends(get_current_auth_user_for_refresh)):
+    access_token = create_access_token(user=user)
+    return Token(access_token=access_token)
 
 
 @router.get("/users/me/")
